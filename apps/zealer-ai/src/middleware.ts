592cd7248b76b999126428d388a46b03c2ffe151
Nishanth Murugan <nishanth.murugan@gmail.com>
2024-06-10 08:17:04 +0530

    adding prisma schema

--- apps/zealer-ai/src/middleware.ts
+++ apps/zealer-ai/src/middleware.ts
@@ -1,7 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-    
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/auth(.*)",
+  "/portal(.*)",
+  "/chatbot",
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
