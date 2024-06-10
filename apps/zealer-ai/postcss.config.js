a04c116b83340ed83253f5340a906c11fce0ed28
Nishanth Murugan <nishanth.murugan@gmail.com>
2024-06-10 09:22:55 +0530

    remove zealer-ai

--- apps/zealer-ai/postcss.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("@zealer/ui/postcss.config");
--- apps/zealer-ai/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-import sharedTailwindConfig from "@zealer/ui/tailwind.config";
-
-const config: Config = {
-  ...sharedTailwindConfig,
-};
-
-export default config;
