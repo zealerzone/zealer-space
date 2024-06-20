f5544cfaa82c4987d3fd351e0cb52dc7d6c06ec2
Nishanth Murugan <nishanth.murugan@gmail.com>
2024-06-20 09:18:54 +0530

    commit

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,9 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@zealer/eslint-config/react-internal.js'],
-  parser: '@typescript-eslint/parser',
+  extends: ["@zealer/eslint-config/react-internal.js"],
+  parser: "@typescript-eslint/parser",
   rules: {
-    'no-redeclare': 'off',
+    "no-redeclare": "off",
   },
 };
